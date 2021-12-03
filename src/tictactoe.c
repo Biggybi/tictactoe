f81178c6dd01b33ec6e94e9ccbefd00b5a8f74b3
biggybi <biggybi@protonmail.com>
2021-12-03 11:01:11 +0100

    fix index to mark

--- src/tictactoe.c
+++ src/tictactoe.c
@@ -10,7 +10,7 @@
 #include <string.h>
 #include <bsd/string.h>
 
-void grid_init(s_grid *grid)
+void	grid_init(s_grid *grid)
 {
 	int		i;
 
@@ -19,50 +19,61 @@ void grid_init(s_grid *grid)
 	grid->height = GRIDHEIGHT;
 
 	i = -1;
-	grid->str[grid->len] = 0;
 	while (++i < grid->len)
 		grid->str[i] = '-';
-	i = GRIDWIDTH;
+	i = grid->width;
 	while (i < grid->len)
 	{
 		grid->str[i] = '\n';
-		i += GRIDWIDTH + 1;
+		i += grid->width + 1;
 	}
+	grid->str[grid->len - 1] = 0;
 }
 
-void play_user(s_grid *grid, int cur_turn)
+int		choice_index(s_grid grid, int choice)
+{
+	int		i;
+
+	i = choice + grid.width - 1;
+	i -= grid.height - ((choice - 1) / grid.width);
+	return (i);
+}
+
+void	play_user(s_grid *grid)
 {
 	char	str[100];
 	int		choice;
 
 	str[0] = 0;
 	while (str[0] < '1' || str[0] > '9' || strlen(str) > 1)
-	/* while (str[0] < '1' || str[0] > '9') */
-	{
-		dprintf(1, "turn: %d\nwhere ? [1-9]\n", cur_turn);
 		scanf("%s", str);
-		printf("input: %s\n", str);
-	}
 	choice = str[0] - '0';
-	dprintf(1, "height: %d\nwidth: %d\nchoice: %d\n", grid->height, grid->width, choice);
-	grid->str[choice - (grid->height - (choice / grid->width))] = 'x';
+	/* dprintf(1, "height: %d\nwidth: %d\nchoice: %d\n", grid->height, grid->width, choice); */
+	grid->str[choice_index(*grid, choice)] = 'x';
 }
 
-void play_cpu(s_grid *grid);
+void	play_cpu(s_grid *grid)
+{
+	grid->str[choice_index(*grid, rand() % 10)] = 'o';
+}
 
-void play(s_grid *grid)
+void	play(s_grid *grid)
 {
 	int		n_turn;
 	int		cur_turn;
+	void	(*player1)(s_grid*);
+	void	(*player2)(s_grid*);
 
-	n_turn = GRIDWIDTH * GRIDHEIGHT;
+	player1 = &play_user;
+	player2 = &play_cpu;
+	n_turn = grid->width * grid->height;
+	dprintf(1, "n_turn: %d\n", n_turn);
 	cur_turn = -1;
 	while (++cur_turn < n_turn)
 	{
-		dprintf(1, "c\n");
-		play_user(grid, cur_turn);
-		/* play_cpu(grid); */
-		/* usleep(300); */
+		dprintf(1, "==== turn: %d ====\n", cur_turn + 1);
+		player1(grid);
+		player2(grid);
 		dprintf(1, "%s\n", grid->str);
 	}
 }
@@ -70,9 +81,11 @@ void play(s_grid *grid)
 int main(void)
 {
 	s_grid		grid;
-	grid.len = (GRIDWIDTH + 1) * GRIDHEIGHT;
 
 	grid_init(&grid);
+	dprintf(1, "grid->width: %d\n", grid.width);
+	dprintf(1, "grid->height: %d\n", grid.height);
+	dprintf(1, "grid->total: %d\n", grid.len);
 	dprintf(1, "%s\n", grid.str);
 	play(&grid);
 	return (0);

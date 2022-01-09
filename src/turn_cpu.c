d5b58456f0347c7f5dfdce62a2e93765c2becd46
tris <tristan.kapous@protonmail.com>
2022-01-09 23:52:16 +0100

    refactor(all): norm

--- src/turn_cpu.c
+++ src/turn_cpu.c
@@ -16,12 +16,12 @@ static int		minimax(char grid[], int player)
 	i = -1;
 	while (++i < GRIDSIZE)
 	{
-		if(grid[i] == GRIDEMPTY)
+		if (grid[i] == GRIDEMPTY)
 		{
 			grid[i] = get_symbol(player);
 			newscore = -minimax(grid, player*-1);
 			grid[i] = GRIDEMPTY;
-			if(newscore > score)
+			if (newscore > score)
 			{
 				score = newscore;
 				move = i;
@@ -43,12 +43,12 @@ static int		move_cpu(char grid[], int player)
 	i = -1;
 	while (++i < GRIDSIZE)
 	{
-		if(grid[i] == GRIDEMPTY)
+		if (grid[i] == GRIDEMPTY)
 		{
 			grid[i] = get_symbol(player);
 			newscore = -minimax(grid, player*-1);
 			grid[i] = GRIDEMPTY;
-			if(newscore > score)
+			if (newscore > score)
 			{
 				score = newscore;
 				move = i;
